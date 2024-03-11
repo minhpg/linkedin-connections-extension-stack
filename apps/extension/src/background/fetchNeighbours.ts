2b378acd56ef1c230473be60e288fffdc690c201
chun1 <arthur@chunai.dev>
2024-03-12 10:04:45 +1100

    working neighbours query

--- apps/extension/src/background/fetchNeighbours.ts
+++ apps/extension/src/background/fetchNeighbours.ts
@@ -3,11 +3,41 @@ import { createFetchConfigs, defaultHeaders } from "./fetchDefaults";
 
 type Depth = "F" | "S" | "O";
 const limit = 100;
+const count = 12;
 const start = 0;
-export async function getConnections(urn_id: string, depth: Depth[] = ["F"]) {
-  // https://www.linkedin.com/voyager/api/graphql?includeWebMetadata=true&variables=(query:(flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:connectionOf,value:List(ACoAABveS-EBYF6A7flrnn_KWGV1AH7_XrTaIME)),(key:network,value:List(F,S)),(key:resultType,value:List(PEOPLE)))))&queryId=voyagerSearchDashFilterClusters.e5b7959b8850da8ff7124e3d6c976359
+// type Response {
+//   data: {
+//     data: {
+//       searchDashClustersByAll: {
+//         metadata: {
+//           totalResultCount: number,
+//         }
+//       }
+//     }
+//   }
+//   included:{
+//     template :"UNIVERSAL",
+//     // TODO
 
+//   } []
+
+// }
+
+export async function getConnections(_urn_id: string, depth: Depth[] = ["F"]) {
+  // https://www.linkedin.com/voyager/api/graphql?includeWebMetadata=true&variables=(query:(flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:connectionOf,value:List(ACoAABveS-EBYF6A7flrnn_KWGV1AH7_XrTaIME)),(key:network,value:List(F,S)),(key:resultType,value:List(PEOPLE)))))&queryId=voyagerSearchDashFilterClusters.e5b7959b8850da8ff7124e3d6c976359
+  // https://www.linkedin.com/voyager/api/graphql?variables=(start:0,origin:MEMBER_PROFILE_CANNED_SEARCH,query:  (flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:connectionOf,value:List(ACoAAB3UX3QBUxAj29smnQhYeyvZlxCGyWHrnb4)),(key:network,value:List(F,S)),(key:resultType,value:List(PEOPLE))),includeFiltersInResponse:false))&queryId=voyagerSearchDashClusters.806ff371aaae722f7d7ecee7a3e83900
+  const urn_id = "ACoAAB3UX3QBUxAj29smnQhYeyvZlxCGyWHrnb4";
+  // https://www.linkedin.com/voyager/api/graphql?includeWebMetadata=true&variables=(start:0,origin:MEMBER_PROFILE_CANNED_SEARCH,query:(flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:connectionOf,value:List(ACoAABveS-EBYF6A7flrnn_KWGV1AH7_XrTaIME)),(key:network,value:List(F,S)),(key:resultType,value:List(PEOPLE))),includeFiltersInResponse:false))&queryId=voyagerSearchDashFilterClusters.e5b7959b8850da8ff7124e3d6c976359&origin=MEMBER_PROFILE_CANNED_SEARCH&start=0&count=12
+  // https://www.linkedin.com/voyager/api/graphql?includeWebMetadata=true&
+  // variables=(start:0,count:10,origin:MEMBER_PROFILE_CANNED_SEARCH,query:(flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:connectionOf,value:List(ACoAAB3UX3QBUxAj29smnQhYeyvZlxCGyWHrnb4)),(key:network,value:List(F,S)),(key:resultType,value:List(PEOPLE))),includeFiltersInResponse:false))&queryId=voyagerSearchDashFilterClusters.e5b7959b8850da8ff7124e3d6c976359&origin=MEMBER_PROFILE_CANNED_SEARCH
+
+  // https://www.linkedin.com/voyager/api/graphql?variables=(start:0,origin:MEMBER_PROFILE_CANNED_SEARCH,query:(flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:connectionOf,value:List(ACoAAB3UX3QBUxAj29smnQhYeyvZlxCGyWHrnb4)),(key:network,value:List(F,S)),(key:resultType,value:List(PEOPLE))),includeFiltersInResponse:false))&queryId=voyagerSearchDashFilterClusters.e5b7959b8850da8ff7124e3d6c976359
   const rliQueries = {
+    // increment this
+    start: 0,
+    // count default is 10
+    // count: 10,
+    origin: "MEMBER_PROFILE_CANNED_SEARCH",
     query: {
       flagshipSearchIntent: "SEARCH_SRP",
       queryParameters: [
@@ -18,30 +48,23 @@ export async function getConnections(urn_id: string, depth: Depth[] = ["F"]) {
         { key: "network", value: depth },
         { key: "resultType", value: ["PEOPLE"] },
       ],
+      includeFiltersInResponse: false,
     },
   };
   const queryParams = {
-    includeWebMetadata: "true",
+    // includeWebMetadata: "true",
     variables: jsToRestli(rliQueries),
-    queryId: "voyagerSearchDashFilterClusters.e5b7959b8850da8ff7124e3d6c976359",
-    origin: "MEMBER_PROFILE_CANNED_SEARCH",
-    // q: "search", // can be all
-    // may need start
+    queryId: "voyagerSearchDashClusters.806ff371aaae722f7d7ecee7a3e83900",
+    // origin: "MEMBER_PROFILE_CANNED_SEARCH",
+    // start: start.toString(),
+    // count: count.toString(),
   };
 
-  // const referrerParams = Object.fromEntries(
-  //   queryParameters.map((item) => [item.key, item.value]),
-  // );
-  // const referredParams = {
-  //   referrerPolicy: "strict-origin-when-cross-origin",
-  //   origin: queryParams.origin,
-  // };
-  // https://www.linkedin.com/search/results/people/?connectionOf=["ACoAABveS-EBYF6A7flrnn_KWGV1AH7_XrTaIME"]&network=["F","S"]&origin=MEMBER_PROFILE_CANNED_SEARCH&sid=~zu
-  // const base ="https://www.linkedin.com/search/results/people/?connectionOf="
   const fetch_params = {
-    referrer: encodeURI(
-      `https://www.linkedin.com/search/results/people/?connectionOf=["${urn_id}"]&network=[${depth.toString()}]&origin=${queryParams.origin}`,
-    ),
+    // referrer: encodeURI(
+    //   `https://www.linkedin.com/search/results/people/?connectionOf=["${urn_id}"]&network=[${depth.toString()}]&origin=${queryParams.origin}`,
+    // ),
+    referrer: "https://www.linkedin.com/mynetwork/",
     referrerPolicy: "strict-origin-when-cross-origin",
     body: null,
     method: "GET",
@@ -54,18 +77,23 @@ export async function getConnections(urn_id: string, depth: Depth[] = ["F"]) {
     `https://www.linkedin.com/voyager/api/graphql?${Object.entries(queryParams)
       .map(([key, value]) => `${key}=${value}`)
       .join("&")}`,
+    // "https://www.linkedin.com/voyager/api/graphql?variables=(start:0,origin:MEMBER_PROFILE_CANNED_SEARCH,query:(flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:connectionOf,value:List(ACoAAB3UX3QBUxAj29smnQhYeyvZlxCGyWHrnb4)),(key:network,value:List(F,S)),(key:resultType,value:List(PEOPLE))),includeFiltersInResponse:false))&queryId=voyagerSearchDashClusters.806ff371aaae722f7d7ecee7a3e83900",
+
     {
       headers: defaultHeaders,
       ...createFetchConfigs(),
       ...fetch_params,
     },
   );
+  console.log(a);
   const data = await a.json();
+  debugger;
   console.log(data);
+
   return data;
 }
 
-export function jsToRestli(obj: Record<string, object>): string {
+export function jsToRestli(obj: any /*Record<string, object*/): string {
   const stringified = JSON.stringify(obj);
   const replaced = stringified
     .replace(/\"/g, "")
